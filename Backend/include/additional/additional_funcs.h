ab19c517741e37523cd8a978e353e88657c29a4e
laytsyn <.>
2024-10-22 20:10:03 +0300

    Переименование папки

--- Backend/include/additional/additional_funcs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <fstream>
-#include <utility>
-
-std::pair<std::string, std::string> GetMethodAndURL(const std::string& request);
-std::string                         ReadFile       (const std::string& path);
