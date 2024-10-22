86f8357df2105b1caeecfeb5e679a4ed1e5f1d59
laytsyn <.>
2024-10-22 20:10:17 +0300

    Переименование папки

---
+++ backend/src/additional/additional_funcs.cpp
@@ -0,0 +1,28 @@
+#include "additional/additional_funcs.h"
+#include <utility>
+
+std::pair<std::string, std::string> GetMethodAndURL(const std::string& request){
+  std::string method, url, HEADERS;
+  HEADERS = request.substr(0, 256);
+  std::size_t posdelim;
+  posdelim = HEADERS.find(" ");
+  method = HEADERS.substr(0, posdelim);
+  HEADERS.erase(0, posdelim + 1);
+  posdelim = HEADERS.find(" ");
+  url = HEADERS.substr(0, posdelim);
+  
+  return { method, url };
+}
+
+std::string ReadFile(const std::string& path){
+  std::string   response = "";
+  std::ifstream inf(path);
+  if(!inf.is_open())
+    return "";
+
+  std::string line;
+  for(;inf >> line;)
+    response += line;
+
+  return response;
+}
