ec193e337d642fec3f4b78957ac01b76e2c8d3ba
Simon <semyon_konovalov_sw@mail.ru>
2024-10-24 23:42:14 +0300

    BCS-19 Исправлен вставка запрос для основной таблицы

--- database/postgres/scripts/inserts/insert_test_data.sql
+++ database/postgres/scripts/inserts/insert_test_data.sql
@@ -13,57 +13,62 @@ year_publish = 1455...current year
 
 
 INSERT INTO main.book(
-	id_library, id_genre, 
-	id_lang, id_binding, 
-	id_ar, title, 
-	author, description, 
-	publisher, book_serias, 
-	year_publish)
-	VALUES (20, 12,
-	17, 6,
+	id_library,	id_binding,
+	id_ar, title,
+	author, description,
+	publisher, book_serias,
+	year_publish,
+	ids_genres, ids_langs)
+	VALUES (
+	20, 6,
 	5, 'Сказки дедушки Мороза', 
 	'Александр Пушкин', 'Сборник классических русских сказок, адаптированных для детей младшего возраста', 
 	'Малыш (АСТ)', 'Книжки-панорамки',
-	1845);
+	1845,
+	main.erase_non_genres('{8, 10, 14}'),
+	main.erase_non_langs('{16}'));
 
 INSERT INTO main.book(
-	id_library, id_genre, 
-	id_lang, id_binding, 
-	id_ar, title, 
-	author, description, 
-	publisher, book_serias, 
-	year_publish)
-	VALUES (21, 11,
-	18, 6,
+	id_library,	id_binding,
+	id_ar, title,
+	author, description,
+	publisher, book_serias,
+	year_publish, 
+	ids_genres, ids_langs)
+	VALUES (21, 6,
 	1, 'Приключения кота Матроскина', 
 	'Эдуард Успенский', 'История о приключениях кота Матроскина и его друзей', 
 	'Росмэн', 'Панорамки',
-	2008);
+	2008,
+	main.erase_non_genres('{8, 10, 14}'::integer[]),
+	main.erase_non_langs('{16, 18}'));
 
 	INSERT INTO main.book(
-	id_library, id_genre, 
-	id_lang, id_binding, 
-	id_ar, title, 
-	author, description, 
-	publisher, book_serias, 
-	year_publish)
-	VALUES (19, 15,
-	16, 7,
+	id_library,	id_binding,
+	id_ar, title,
+	author, description,
+	publisher, book_serias,
+	year_publish, 
+	ids_genres, ids_langs)
+	VALUES (19, 7,
 	5, 'Сказки на ночь', 
 	'Ганс Христиан Андерсен', 'Сборник знаменитых сказок датского писателя, адаптированных для детей', 
 	'Антураж', 'Сказка в окошке',
-	2003);
+	2003,
+	main.erase_non_genres('{8, 16}'),
+	main.erase_non_langs('{17, 18}'));
 
 	INSERT INTO main.book(
-	id_library, id_genre, 
-	id_lang, id_binding, 
-	id_ar, title, 
-	author, description, 
-	publisher, book_serias, 
-	year_publish)
-	VALUES (20, 12,
-	18, 7,
+	id_library,	id_binding,
+	id_ar, title,
+	author, description,
+	publisher, book_serias,
+	year_publish,
+	ids_genres, ids_langs)
+	VALUES (20, 7,
 	3, '100 окошек — открывай-ка!', 
 	'Тони Вульф.', 'Книга с окошками, где за створками спрятаны интересные факты и загадки', 
 	'Феникс', 'Книжка с окошками',
-	2012);
+	2012,
+	main.erase_non_genres('{10, 14}'),
+	main.erase_non_langs('{18}'));
